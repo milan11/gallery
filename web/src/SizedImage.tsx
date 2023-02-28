8bd3d545d89b806dca17f76034754202ddbd7cdf
Milan Laslop <milan33@gmail.com>
2023-02-28 18:14:02 +0100

    we will not support images without specified dimensions anymore

--- web/src/SizedImage.tsx
+++ web/src/SizedImage.tsx
@@ -1,31 +1,27 @@
 type Props = {
   src: string;
-  width: number | null;
-  height: number | null;
+  width: number;
+  height: number;
   className: string;
 };
 
 export const SizedImage = (props: Props) => {
-  if (props.width && props.height) {
-    return (
-      <svg
-        viewBox={`0 0 ${props.width} ${props.height}`}
-        className={props.className}
-        style={{
-          maxWidth: `min(${props.width}px, 100%)`,
-          maxHeight: `min(${props.height}px, 100%)`,
-        }}
-      >
-        <image
-          x={0}
-          y={0}
-          width={props.width}
-          height={props.height}
-          href={props.src}
-        ></image>
-      </svg>
-    );
-  } else {
-    return <img className={props.className} src={props.src} alt="" />;
-  }
+  return (
+    <svg
+      viewBox={`0 0 ${props.width} ${props.height}`}
+      className={props.className}
+      style={{
+        maxWidth: `min(${props.width}px, 100%)`,
+        maxHeight: `min(${props.height}px, 100%)`,
+      }}
+    >
+      <image
+        x={0}
+        y={0}
+        width={props.width}
+        height={props.height}
+        href={props.src}
+      ></image>
+    </svg>
+  );
 };
