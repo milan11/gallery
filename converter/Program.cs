30fc35162a87391a5830dc5275a04c6c4ec2a8d9
Milan Laslop <milan33@gmail.com>
2023-03-05 16:20:38 +0100

    do not split the photos to portrait and landscape in the converter - we will re-add the sorting to the web app later

--- converter/Program.cs
+++ converter/Program.cs
@@ -37,9 +37,7 @@ public static class Converter
 
         GalleryData data = new GalleryData(prevData.Title);
 
-        List<Photo> photos_almostSquare = new List<Photo>();
-        List<Photo> photos_landscape = new List<Photo>();
-        List<Photo> photos_portrait = new List<Photo>();
+        List<Photo> photos = new List<Photo>();
 
         ImageCodecInfo encoder = GetEncoderInfo(ImageFormat.Jpeg);
 
@@ -68,31 +66,10 @@ public static class Converter
                 (uint)newImage_s.Height
                 );
 
-            if (IsAlmostSquare(origImage.Width, origImage.Height))
-            {
-                photos_almostSquare.Add(photo);
-            }
-            else if (IsLandscape(origImage.Width, origImage.Height))
-            {
-                photos_landscape.Add(photo);
-            }
-            else
-            {
-                photos_portrait.Add(photo);
-            }
+            photos.Add(photo);
         }
 
-        if (photos_landscape.Count >= photos_portrait.Count)
-        {
-            photos_landscape.AddRange(photos_almostSquare);
-        }
-        else
-        {
-            photos_portrait.AddRange(photos_almostSquare);
-        }
-
-        data.Photos.AddRange(photos_landscape.OrderBy(p => NormalizeFileNameForSorting(p.File)));
-        data.Photos.AddRange(photos_portrait.OrderBy(p => NormalizeFileNameForSorting(p.File)));
+        data.Photos.AddRange(photos.OrderBy(p => NormalizeFileNameForSorting(p.File)));
 
         var options = new JsonSerializerOptions()
         {
@@ -102,18 +79,6 @@ public static class Converter
         };
 
         File.WriteAllText(Path.Combine(targetDir, DataJsonFileName), JsonSerializer.Serialize(data, options), Encoding.UTF8);
-    }
-
-    private static bool IsAlmostSquare(int width, int height)
-    {
-        double ratio = (width >= height) ? ((double)height / (double)width) : ((double)width / (double)height);
-
-        return ratio >= 0.85;
-    }
-
-    private static bool IsLandscape(int width, int height)
-    {
-        return width >= height;
     }
 
     private static string NormalizeFileNameForSorting(string fileName)
